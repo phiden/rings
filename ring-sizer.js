2a2520685a033ac6183d19fcc571ca5d54bbfcfc
Sophia Dengo <sdengo@gmail.com>
2014-12-16 21:01:13 -0800

    revised markup

--- ring-sizer.js
+++ ring-sizer.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-
-	console.log('hello world');
-
+	
+	var thickValue;			//the thickness of material input by the user
+	var thickSystem;		//the system of the thick measurement
+	var inputSize; 			//the ring size input by the user
+	var inputSystem;		//whether the ring size is in US ring sizes or mm
+	var shankSize;			//the number returned for the user
+	
+	
+	$('#calculate').click(function(e) {
+		
+		e.preventDefault();
+		
+		
+		thickValue = $('#thick').val();
+		thickSystem = $('#thickSystem').val();
+		inputSize = $('#size').val();
+		inputSystem = $('#sizeSystem').val();
+		
+		
+		console.log("calculate clicked: ", thickValue, thickSystem, inputSize, inputSystem);
+	})
+	
+	
 })
